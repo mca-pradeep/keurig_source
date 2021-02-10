6809c8da6eebeca680315a482c9f4b6b1811aa76
Pradeep Pai <pradeep@nextwebgen.com>
2021-02-10 20:38:43 +0530

    carousel added

---
+++ src/components/UI/OptionCarousel.jsx
@@ -0,0 +1,45 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 10,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 8,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 5,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
+
+const OptionCarousel = (props) => {
+    return (<Carousel
+        arrows={false}
+        swipeable={true}
+        draggable={false}
+        showDots={false}
+        responsive={responsive}
+        ssr={false} // means to render carousel on server-side.
+        infinite={false}
+        autoPlay={false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType={""}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item">{props.children}</Carousel>)
+
+};
+export default OptionCarousel;
