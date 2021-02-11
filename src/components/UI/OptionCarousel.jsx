59c2633bb1c3b44ec5c4a294cde393c54eedea71
Pradeep Pai <pradeep@nextwebgen.com>
2021-02-11 16:03:28 +0530

    updated

--- src/components/UI/OptionCarousel.jsx
+++ src/components/UI/OptionCarousel.jsx
@@ -1,46 +1,18 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 10,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 8,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 5,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 3,
-  },
-};
+import Carousel from "infinite-react-carousel";
 
 const OptionCarousel = (props) => {
   return (
     <Carousel
+      rows={1}
+      slidesPerRow={1}
+      slidesToShow={3}
+      className="carousel-container"
+      centerMode={true}
+      centerPadding={10}
       arrows={false}
-      swipeable={true}
-      draggable={true}
-      showDots={false}
-      responsive={responsive}
-      ssr={false} // means to render carousel on server-side.
-      infinite={false}
-      autoPlay={false}
-      autoPlaySpeed={1000}
-      keyBoardControl={true}
-      customTransition="all .5"
-      transitionDuration={500}
-      containerClass="carousel-container"
-      removeArrowOnDeviceType={["tablet", "mobile"]}
-      deviceType={""}
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item">
+      arrowsBlock={false}
+      //className="carousel-item"
+    >
       {props.children}
     </Carousel>
   );
