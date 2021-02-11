8788d5f9d782fb179ae7d62811736c32e97f3321
Pradeep Pai <pradeep@nextwebgen.com>
2021-02-11 18:20:40 +0530

    updated

--- src/components/UI/OptionCarousel.jsx
+++ src/components/UI/OptionCarousel.jsx
@@ -1,17 +1,29 @@
-import Carousel from "infinite-react-carousel";
-
+//import Carousel from "infinite-react-carousel";
+import Carousel from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const OptionCarousel = (props) => {
+  const settings = {
+    className: "carousel-container",
+    infinite: false,
+    centerPadding: "60px",
+    slidesToShow: 2.5,
+    swipeToSlide: true,
+    appendDots: false,
+    arrows: false,
+  };
   return (
     <Carousel
-      rows={1}
-      slidesPerRow={1}
-      slidesToShow={3}
-      className="carousel-container"
-      centerMode={true}
-      centerPadding={0}
-      arrows={false}
-      arrowsBlock={false}
-    //className="carousel-item"
+      {...settings}
+      // rows={1}
+      // slidesPerRow={1}
+      // slidesToShow={3}
+      // className="carousel-container"
+      // centerMode={true}
+      // centerPadding={0}
+      // arrows={false}
+      // arrowsBlock={false}
+      //className="carousel-item"
     >
       {props.children}
     </Carousel>
